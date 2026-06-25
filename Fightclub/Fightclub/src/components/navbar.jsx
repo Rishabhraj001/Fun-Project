4ff25063ff590d7e57f5f247394429eef1b9ef22
Rishabhraj001 <rishabhraj1130jnvbgp@gmail.com>
2026-06-25 11:17:55 +0530

    fightclub

---
+++ Fightclub/Fightclub/src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-logo">THE FIGHT <span>CLUB</span></div>
+
+      <ul className="nav-links">
+        <li>Tyler </li>
+        <li>Fight club</li>
+        <li>Edward</li>
+        
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
